d9f4c4a8fcee544f4e066988816c85f05f29f19f
keshvadi <webidsina@gmail.com>
2023-03-12 16:35:27 -0700

    added psnr

--- dash-emulator-master/setup.py
+++ dash-emulator-master/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
-
 from setuptools import find_packages
 
 requirements = [
@@ -11,13 +10,12 @@ requirements = [
     "behave"  # for Behavior-Driven Development (BDD)
 ]
 
-setup(name='dash-emulator',
-      version='0.2.0.dev16',
-      description='A headless player to emulate the playback of MPEG-DASH streams',
-      author='Yang Liu',
-      author_email='yang.jace.liu@linux.com',
-      url='https://github.com/Yang-Jace-Liu/dash-emulator',
-      packages=find_packages(),
-      scripts=["scripts/dash-emulator.py"],
-      install_requires=requirements
-      )
+setup(
+    name='dash-emulator',
+    version='0.2.0.dev16',
+    description='A headless player to emulate the playback of MPEG-DASH streams',
+    author='Sina',
+    packages=find_packages(),
+    scripts=["scripts/dash-emulator.py"],
+    install_requires=requirements
+)
